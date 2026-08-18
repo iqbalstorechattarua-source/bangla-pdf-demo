1899b96b2a12429d78304fba82a33f58e27e17ec
iqbalstorechattarua-source <iqbalstorechattarua@gmail.com>
2026-08-18 14:38:35 +0600

    Update server.js

--- server.js
+++ server.js
@@ -32,9 +32,7 @@ app.post("/generate-pdf",async(req,res)=>{
 
     browser=await puppeteer.launch({
   headless:true,
-  executablePath:
-    process.env.PUPPETEER_EXECUTABLE_PATH ||
-    "/opt/render/.cache/puppeteer/chrome/linux-148.0.7778.97/chrome-linux64/chrome",
+  executablePath:puppeteer.executablePath(),
   args:[
     "--no-sandbox",
     "--disable-setuid-sandbox",
